e8365a75300dbefd32e30840a9a581e0ebe4260f
kozyakin <57475371+kozyakin@users.noreply.github.com>
2020-11-04 12:56:24 +0300

    Make comments in English

--- html/my_make4ht.bat
+++ html/my_make4ht.bat
@@ -1,17 +1,17 @@
 @echo off
 chcp 65001 >nul
 echo.
-echo 1. Создание файла %~n1.html
+echo 1. Creation of  %~n1.html
 echo.
 make4ht -sc myconfig.cfg %1 "0,mathjax,p-indent,charset=utf-8" " -cunihtf -utf8"
 echo.
-echo 2. "Подчистка" файла %~n1.html
+echo 2. "Cleaning" of  %~n1.html
 echo.
 perl -p -i.bak -e "s/>,(\xC2\xA0|\x00\xA0|&#xC2A0;|&#x00A0;)</>, </g" %~n1.html | rem
 
-echo Для удаления рабочих файлов программы make4ht нажмите ENTER.
+echo To delete working files of make4ht press ENTER.
 SET choice=
-SET /p choice=Для сохранения рабочих файлов введите любой символ и нажмите ENTER: 
+SET /p choice=To keep  working files of make4ht enter any symbol and press ENTER: 
 IF NOT '%choice%'=='' GOTO exit
 
 perl -e "unlink qw/%~n1.html.bak %~n1.4ct %~n1.4tc %~n1.dvi %~n1.out  %~n1.idv %~n1.igv %~n1.lg %~n1.tmp %~n1.xref/" | rem
