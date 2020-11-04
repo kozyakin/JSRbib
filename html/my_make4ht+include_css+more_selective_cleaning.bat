a1c67e9ea2e8107360dd2eff966cd604c5987b55
kozyakin <57475371+kozyakin@users.noreply.github.com>
2020-11-04 13:04:37 +0300

    Make comments in English

--- html/my_make4ht+include_css+more_selective_cleaning.bat
+++ html/my_make4ht+include_css+more_selective_cleaning.bat
@@ -4,25 +4,32 @@ echo.===========================================================================
 echo                                ВНИМАНИЕ!
 echo.
 echo Для внедрения (с помощью опции css-in) создаваемого в процессе компиляции 
-echo.css-файла в результирующий html-файл в корневой директории должен находиться 
+echo .css-файла в результирующий html-файл в корневой директории должен находиться 
 echo модифицированный файл html5.4ht !
+echo
+echo ----------------------------------------------------------------------------
+echo                                ATTENTION!
+echo.
+echo To embed (using the css-in option) generated at compile-time
+echo .css file to the resulting html file in the root directory should be
+echo modified html5.4ht file!
 echo.============================================================================
 echo.
-echo 1. Создание файла %~n1.html
+echo 1. Creation of %~n1.html
 echo.
 make4ht -sc myconfig.cfg %1 "0,mathjax,p-indent,charset=utf-8" " -cunihtf -utf8"
 echo.
-echo 2. Внедрение css-файла %~n1.css в %~n1.html
+echo 2. Embedding css-file %~n1.css in %~n1.html
 echo.
 make4ht -sc myconfig.cfg -m draft %1 "0,mathjax,p-indent,charset=utf-8,css-in" " -cunihtf -utf8"
 echo.
-echo 3. "Подчистка" файла %~n1.html
+echo 3. "Cleaning" of %~n1.html
 echo.
 perl -p -i.bak -e "s/<\/a>,(\xC2\xA0|\x00\xA0|&#xC2A0;|&#x00A0;)<a/<\/a>, <a/g";  -e "s/,(\xC2\xA0|\x00\xA0|&#xC2A0;|&#x00A0;)<\/span>/, <\/span>/g"; -e "s/.figure \&gt; p/.figure \> p/g" %~n1.html | rem
 
-echo Для удаления рабочих файлов программы make4ht нажмите ENTER.
+echo To delete working files of make4ht press ENTER.
 SET choice=
-SET /p choice=Для сохранения рабочих файлов введите любой символ и нажмите ENTER: 
+SET /p choice=To keep  working files of make4ht enter any symbol and press ENTER:
 IF NOT '%choice%'=='' GOTO exit
 
 perl -e "unlink qw/%~n1.html.bak %~n1.4ct %~n1.4tc %~n1.dvi %~n1.out  %~n1.idv %~n1.igv %~n1.lg %~n1.tmp %~n1.xref/" | rem
