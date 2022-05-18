226f3a660ee339d39c61c670c5dac45720ce2417
kozyakin <57475371+kozyakin@users.noreply.github.com>
2022-05-18 17:05:30 +0300

    Highlighting some info

--- html/my_make4ht+include_css+more_selective_cleaning.bat
+++ html/my_make4ht+include_css+more_selective_cleaning.bat
@@ -11,25 +11,25 @@ echo ---------------------------------------------------------------------------
 echo                                ATTENTION!
 echo.
 echo To inject (with the css-in option) the css-file created during the 
-echo compilation process into the resulting html-file, the [93mthe root directory 
+echo compilation process into the resulting html-file, [93mthe root directory 
 echo must contain a modified html5.4ht file[0m !
 echo.============================================================================
 echo.
-echo 1. Creation of %~n1.html
+echo [92m1. Creation of %~n1.html[0m
 echo.
 make4ht -sc myconfig.cfg %1 "0,mathjax,p-indent,charset=utf-8" " -cunihtf -utf8"
 echo.
-echo 2. Embedding css-file %~n1.css in %~n1.html
+echo [92m2. Embedding css-file %~n1.css in %~n1.html[0m
 echo.
 make4ht -sc myconfig.cfg -m draft %1 "0,mathjax,p-indent,charset=utf-8,css-in" " -cunihtf -utf8"
 echo.
-echo 3. "Cleaning" of %~n1.html
+echo [92m3. "Cleaning" of %~n1.html[0m
 echo.
 perl -p -i.bak -e "s/<\/a>,(\xC2\xA0|\x00\xA0|&#xC2A0;|&#x00A0;)<a/<\/a>, <a/g";  -e "s/,(\xC2\xA0|\x00\xA0|&#xC2A0;|&#x00A0;)<\/span>/, <\/span>/g"; -e "s/.figure \&gt; p/.figure \> p/g" %~n1.html | rem
 
 echo To delete working files of make4ht press ENTER.
 SET choice=
-SET /p choice=To keep  working files of make4ht enter any symbol and press ENTER:
+SET /p choice=To keep working files of make4ht enter any symbol and press ENTER:
 IF NOT '%choice%'=='' GOTO exit
 
 perl -e "unlink qw/%~n1.html.bak %~n1.4ct %~n1.4tc %~n1.dvi %~n1.out  %~n1.idv %~n1.igv %~n1.lg %~n1.tmp %~n1.xref/" | rem
--- html/my_make4ht.bat
+++ html/my_make4ht.bat
@@ -1,11 +1,11 @@
 @echo off
 chcp 65001 >nul
 echo.
-echo 1. Creation of  %~n1.html
+echo [92m1. Creation of %~n1.html[0m
 echo.
 make4ht -sc myconfig.cfg %1 "0,mathjax,p-indent,charset=utf-8" " -cunihtf -utf8"
 echo.
-echo 2. "Cleaning" of  %~n1.html
+echo [92m2. "Cleaning" of %~n1.html[0m
 echo.
 perl -p -i.bak -e "s/>,(\xC2\xA0|\x00\xA0|&#xC2A0;|&#x00A0;)</>, </g" %~n1.html | rem
 
