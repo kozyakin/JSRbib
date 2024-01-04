a1d091cfada4ae13dd2283924fcd28bbab8e4ff5
Victor Kozyakin <57475371+kozyakin@users.noreply.github.com>
2024-01-04 15:03:13 +0300

    sed replaced by powershell

--- html/cleaning.cmd
+++ html/cleaning.cmd
@@ -1,20 +1,20 @@
 @echo off
 chcp 65001 >nul
 echo.
-sed -i "s/>,\(\xC2\xA0\|\x00\xA0\)</>, </g;s/>\xC2\xA0<\/span><a/> <\/span><a/g;s/.figure &gt; p/.figure \> p/g" %~n1.html | rem
+call th.bat %~n1.html
+powershell -ExecutionPolicy Bypass -Command "Set-Content %~n1.html -Value (Get-Content %~n1.html | ForEach-Object {$_ -replace '>,&nbsp;<','>, <'})"
 
 echo To delete working files of make4ht press ENTER.
 SET choice=
 SET /p choice=To keep working files of make4ht enter any symbol and press ENTER:
 IF NOT '%choice%'=='' GOTO exit
 
-del /S /F /Q "%~n1.html.bak"
 xcopy /Y *.html  %TEMP%\%~n1\ 
 xcopy /Y *.svg  %TEMP%\%~n1\ 
 make4ht -m clean -a info %1
 xcopy /Y %TEMP%\%~n1\  .\ 
-rd /S /Q  %TEMP%\%~n1  
-call th.bat %~n1.html
+rd /S /Q  %TEMP%\%~n1 
+call th.bat %~n1.html 
 pause
 ::exit
 exit
