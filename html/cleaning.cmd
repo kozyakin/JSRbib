d47e669cf1208b3a5a590b5ce29eea09eb8eac68
Victor Kozyakin <57475371+kozyakin@users.noreply.github.com>
2024-01-05 11:07:50 +0300

    Update cleaning.cmd

--- html/cleaning.cmd
+++ html/cleaning.cmd
@@ -1,16 +1,15 @@
 @echo off
 chcp 65001 >nul
-echo To clean %~n1.html file press ENTER.
-SET /p choice=
+SET /p choice=[93mTo start cleaning %~n1.html file press ENTER: [0m
 
 call th.bat %~n1.html
 powershell -ExecutionPolicy Bypass -Command "Set-Content %~n1.html -Value (Get-Content %~n1.html | ForEach-Object {$_ -replace '>,&nbsp;<','>, <'})"
 call th.bat %~n1.html 
 
 echo.
-SET /p choice=To keep working files of make4ht enter any symbol and press ENTER:
+SET /p choice=[93mTo keep working files of make4ht enter any symbol and press ENTER: [0m
 IF NOT '%choice%'=='' GOTO exit
-
+echo.
 xcopy /Y *.html  %TEMP%\%~n1\ 
 xcopy /Y *.svg  %TEMP%\%~n1\ 
 make4ht -m clean -a info %1
