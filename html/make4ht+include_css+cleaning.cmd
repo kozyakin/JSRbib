8df861d90d52d98e331ec7e7c7f086e94a1cf3eb
Victor Kozyakin <57475371+kozyakin@users.noreply.github.com>
2024-01-09 15:21:47 +0300

    Update make4ht+include_css+cleaning.cmd

--- html/make4ht+include_css+cleaning.cmd
+++ html/make4ht+include_css+cleaning.cmd
@@ -24,17 +24,17 @@ set "infile=%~f1"
 set "infile=%infile:\=/%"
 echo [92m1. Creation of %~n1.html[0m
 echo.
-make4ht -sm draft %infile% "myconfig" " -cunihtf -utf8"
+make4ht.exe -sm draft %infile% "myconfig" " -cunihtf -utf8"
 echo.
 echo Creating %~n1.bbl file
 del /S /Q /F %~n1.bbl
 bibtexu -H -l ru -o ru %~n1
 echo.
-make4ht -s %infile% "myconfig" " -cunihtf -utf8"
+make4ht.exe -s %infile% "myconfig" " -cunihtf -utf8"
 echo.
 echo [92m2. Embedding css-file %~n1.css in %~n1.html[0m
 echo.
-make4ht -sm draft %infile% "myconfig" " -cunihtf -utf8"
+make4ht.exe -sm draft %infile% "myconfig" " -cunihtf -utf8"
 echo.
 echo [92m3. "Cleaning" of %~n1.html[0m
 echo.
@@ -45,7 +45,7 @@ echo [94m...tidy first pass...[0m
 call tidy-html5.bat %~n1.html
 echo.
 echo [94m...powershell search/replace pass...[0m
-powershell -ExecutionPolicy Bypass -Command "Set-Content %~n1.html -Value (Get-Content %~n1.html | ForEach-Object {$_ -replace '>,&nbsp;<','>, <'})"
+powershell.exe -ExecutionPolicy Bypass -Command "Set-Content %~n1.html -Value (Get-Content %~n1.html | ForEach-Object {$_ -replace '>,&nbsp;<','>, <'})"
 echo.
 echo [94m...tidy second pass...[0m
 call tidy-html5.bat --show-info no %~n1.html 
@@ -56,7 +56,7 @@ IF NOT '%choice%'=='' GOTO exit
 echo.
 xcopy /Y *.html  %TEMP%\%~n1\ 
 xcopy /Y *.svg  %TEMP%\%~n1\ 
-make4ht -m clean -a info %1
+make4ht.exe -m clean -a info %1
 xcopy /Y %TEMP%\%~n1\  .\ 
 rd /S /Q  %TEMP%\%~n1 
 
