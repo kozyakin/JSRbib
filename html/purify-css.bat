9e9ccacc58939dc75a694aa29cfe50c90dc00e0f
Victor Kozyakin <57475371+kozyakin@users.noreply.github.com>
2024-11-01 15:56:58 +0300

    Added option to remove duplicates in css

--- html/purify-css.bat
+++ html/purify-css.bat
@@ -1,4 +1,4 @@
 @echo off
 @call purifycss.cmd %1 %2 -o %1
-@call cleancss.cmd --format keep-breaks -o %1.clean %1 
+@call cleancss.cmd -O2 --format keep-breaks -o %1.clean %1 
 if exist %1.clean move %1.clean %1
