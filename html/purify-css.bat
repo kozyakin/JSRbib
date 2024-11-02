13a954e789ce0925f50068311da590e565adc7b3
Victor Kozyakin <57475371+kozyakin@users.noreply.github.com>
2024-11-02 18:08:54 +0300

    Update purify-css.bat

--- html/purify-css.bat
+++ html/purify-css.bat
@@ -1,4 +1,4 @@
 @echo off
-@cmd /c purifycss.cmd %1 %2 -o pur-%1
-@cmd /c cleancss.cmd -O2 --format keep-breaks -o %1 pur-%1
-del pur-%1
+@cmd /c purifycss.cmd "%1" "%2" -o "%TEMP%\%1"
+@cmd /c cleancss.cmd -O2 --format keep-breaks -o "%1" "%TEMP%\%1"
+del /Q "%TEMP%\%1"
