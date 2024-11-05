0a51780c39c326326350efeaf6bf235eae09e25c
Victor Kozyakin <57475371+kozyakin@users.noreply.github.com>
2024-11-05 16:21:00 +0300

    Update inject-css.js

--- html/inject-css.js
+++ html/inject-css.js
@@ -6,7 +6,7 @@ const filehtml = process.argv[3];
 const html = fs.readFileSync(filehtml, 'utf8');
 const css = fs.readFileSync(filecss, 'utf8');
 
-var spattern = new RegExp("<link .*href='" + filecss + "'.*>");
+const spattern = new RegExp("<link .*href='" + filecss + "'.*>");
 const rpattern = "<style type='text/css'>\n\n<!--\n" + css + "\n\n-->\n</style>";
 
 const injectedHtml = html.replace(spattern,rpattern);
