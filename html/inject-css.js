a4dc7261973437331284e35c1103be89d06f6918
Victor Kozyakin <57475371+kozyakin@users.noreply.github.com>
2024-11-06 16:30:52 +0300

    Update inject-css.js

--- html/inject-css.js
+++ html/inject-css.js
@@ -15,5 +15,5 @@ if (injectedHtml) {
 	fs.writeFileSync(filehtml, injectedHtml);
 	console.log("\n[94mInject-CSS: done![0m");
 } else {
-	console.log("\n[94mInject-CSS: Sorry! Something went wrong![0m");
+	console.log("\n[91mInject-CSS: Sorry! Something went wrong![0m");
 }
