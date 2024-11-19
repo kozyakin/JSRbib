0b9b1e3c9dea8e67e668e45bf8df9b6686c31282
Victor Kozyakin <57475371+kozyakin@users.noreply.github.com>
2024-11-19 11:44:31 +0300

    Update inject-css.lua

--- html/inject-css.lua
+++ html/inject-css.lua
@@ -1,23 +1,21 @@
 filecss = arg[1]
 filehtml = arg[2]
 
-local function readFile(filename)
-    local f = io.open(filename, "r")
-    if not f then return nil end
-    local content = f:read("*a")
-    f:close()
-    return content
+local function readfile(name)
+  local f = assert(io.open(name, "r"))
+  local s = f:read("*a")
+  f:close()
+  return s
 end
 
-local function writeFile(file, data)
-    local f = io.open(file, "w")
-    if not f then return nil end
-    f:write(data)
-    f:close()
+local function writefile(name, sum)
+  local f = assert(io.open(name, "w"))
+  f:write(sum)
+  f:close()
 end
 
-html = readFile(filehtml)
-css = readFile(filecss)
+html = readfile(filehtml)
+css = readfile(filecss)
 css = css:gsub('%%', '%%%%')
 
 spattern = "<link .*href='" .. filecss .. "'.-/>"
@@ -27,7 +25,7 @@ injectedHtml = html:gsub(spattern, rpattern)
 injectedHtml = injectedHtml:gsub('%%%%','%%')
 
 if injectedHtml then
-    writeFile(filehtml, injectedHtml)
+    writefile(filehtml, injectedHtml)
     print("\n\x1b[94mInject-CSS: done!\x1b[0m")
 else
     print("\n\x1b[91mInject-CSS: Sorry! Something went wrong!\x1b[0m")
