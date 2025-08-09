a35a1a6d59f843d386b8b420fc005bc142ea7858
Victor Kozyakin <57475371+kozyakin@users.noreply.github.com>
2025-08-09 10:33:20 +0300

    Update compilation scripts

--- html/make4ht+inject-css-lua.cmd
+++ html/make4ht+inject-css-lua.cmd
@@ -60,8 +60,7 @@ echo %blue%tidy-html5 pass...%reset%
 call tidy-html5.cmd %~n1.html 
 echo.
 echo %blue%powershell search/replace pass...%reset%
-powershell.exe -ExecutionPolicy Bypass -Command "Set-Content %~n1.html -Value (Get-Content %~n1.html | ForEach-Object {$_ -replace '>,&nbsp;<','>, <'})"
-dos2unix %~n1.html
+powershell.exe -ExecutionPolicy Bypass -Command "Set-Content %~n1.html -Value (Get-Content -Raw %~n1.html | ForEach-Object {$_ -replace '>,&nbsp;<','>, <'})"
 echo.
 set choice=
 set /p "choice=%yellow%To keep working files of make4ht press any key and then ENTER: %reset%"
