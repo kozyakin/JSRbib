f19d47422273ffe1bb23fa3b91446ff7b4252c37
Victor Kozyakin <57475371+kozyakin@users.noreply.github.com>
2025-12-01 16:35:22 +0300

    Update tidy-html5.cmd

--- html/tidy-html5.cmd
+++ html/tidy-html5.cmd
@@ -1,2 +1,2 @@
 @echo.
-@tidy --doctype html5 --output-html yes --char-encoding utf8 --newline lf --clean yes --indent auto --tidy-mark no --wrap 0 --write-back yes %1 %2 %3 %4 %5 %6
+@tidy --doctype html5 --output-html yes --char-encoding utf8 --newline lf --clean yes --indent auto --tidy-mark no --show-warnings no --wrap 0 --write-back yes %1 %2 %3 %4 %5 %6
